a1c7ecfb493a9d10aa814ca895caea5baa5651a9
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-16 00:24:08 -0400

    Update CLI/menu-1

--- keynox/cli.py
+++ keynox/cli.py
@@ -3,48 +3,67 @@ import os
 
 from time import sleep
 
+from vault import Vault
 
-def show_logo():
+
+def show_logo() -> None:
 	# Displays the Keynox logo
-	print(open('./cli/keynox-logo.ascii').read())
+	with open('./cli/keynox-logo.ascii') as file:
+		print(file.read())
 
-def show_menu(level: int=0):
+def show_menu(level: int) -> None:
 	# Displays the menu at the specified level
-	print(open(f'./cli/menu/menu-{level}.x').read())
+	with open(f'./cli/menu/menu-{level}.x') as file:
+		print(file.read())
 
 
-# reboot section
-clear = ["clear", "cls"][os.name == "nt"]
-defcode, redcode = '\033[0m', '\033[91m'
-
-
-def menu():
-	level = 0
-	errorlevel = 0
+def menu() -> None:
+	level = errorlevel = 0
 	while True:
-		os.system(clear)
-		show_logo()
-		show_menu(level)
-
-		if errorlevel == 1:
-			sleep(0.1)
-			print(f"{redcode}[!] Invalid option. Please try again.\n{defcode}")
-			errorlevel = 0
-		choice = input("> ")
-		if choice.lower() in ("0", "exit", "quit"):
-			break
+		os.system(CLEAR); show_logo(); show_menu(level)
 
 		if level == 0:
-			if choice == "1":
-				pass #create
+			if errorlevel == 1:
+				errorlevel = 0
+				sleep(0.1)
+				print(f"{redcode}[!] Invalid option. Please try again.\n{defcode}")
+
+			choice = input("> ")
+
+			if choice.lower() in ("0", "exit", "quit"):
+				break
+			if choice.lower() in ("?", "help"):
+				print("\n#? help"); sleep(0.5)
+
+			elif choice == "1":
+				level = 1
 			elif choice == "2":
-				pass #import
+				level = 2
 			else:
 				errorlevel = 1
-		else:
-			pass
-	return 0;
 
-def main(): menu()
+		elif level == 1:
+			print("Enter file in which to save the vault:")
+			try:
+				file_path = input("\t> ")
+				if os.path.isfile(file_path):
+					choice = input("The file already exists. Overwrite (y/N): ") or "no"
+					if choice.lower() in ("y", "yes"):
+						pass
+				else:
+					pass
+			except:
+				level = 0
+
+# reboot section
+CLEAR = "cls" if os.name == "nt" else "clear"
+defcode, redcode = '\033[0m', '\033[91m'
+
+def main():
+	try:
+		menu()
+	except:
+		print()
+		pass
 
 if __name__ == "__main__": main()
