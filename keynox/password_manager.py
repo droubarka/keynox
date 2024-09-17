3c4e42cde520aa1b927ac9de616c831bdfc09624
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-17 00:00:41 -0400

    Upgrade ALL

--- keynox/password_manager.py
+++ keynox/password_manager.py
@@ -3,12 +3,3 @@ class PasswordManager:
 	def __init__(self, master_password):
 		self.master_password = master_password
 		pass
-
-	def unknown0():
-		pass
-
-	def unknown1():
-		pass
-
-	def unknown2():
-		pass
