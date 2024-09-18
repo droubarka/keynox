288d391c7ac5c9afe199e1f5f8d75d81efaca550
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-18 05:59:15 -0400

    Finish CLI/MENU-0 to CLI/MENU-2

--- keynox/cli.py
+++ keynox/cli.py
@@ -8,18 +8,34 @@ from vault import Vault
 CLEAR = "cls" if os.name == "nt" else "clear"
 defcode, redcode = '\033[0m', '\033[91m'
 
-def show_error(errorlevel: int, *args, **kwargs) -> int:
-	if errorlevel == 0:
-		sleep(0.1)
-		print(f"{redcode}[!] Invalid option. Please try again.\n", file=sys.stderr)
-	elif errorlevel == 100:
-		print(f"{redcode}{kwargs['filename']} already exists.", file=sys.stderr)
+def show_error(errorlevel: int, **args) -> int:
+
+	print(f"{redcode}", end='', file=sys.stderr)
+
+	# level-0
+	if errorlevel == 100:
+		print("[!] Invalid option. Please try again.", file=sys.stderr)
+
+	# level-1
 	elif errorlevel == 200:
-		sleep(0.1)
-		print(f"{redcode}[!] `{kwargs['filename']}` file not found. Please try again.\n", file=sys.stderr)
-	else:
-		input("XXX")
-	print(defcode, end='')
+		print("\n[!] The file already exists.", file=sys.stderr)
+	elif errorlevel == 201:
+		print("[!] FileNotFoundError: `{file}`".format(file=args["filename"]), file=sys.stderr)
+	elif errorlevel == 202:
+		print("[!] PermissionError: `{file}`".format(file=args["filename"]), file=sys.stderr)
+	elif errorlevel == 203:
+		print("[!] IsADirectoryError: `{file}`".format(file=args["filename"]), file=sys.stderr)
+
+	# level-2
+	elif errorlevel == 300:
+		printf("", file=sys.stderr)
+
+	# level-x
+	if errorlevel == -99:
+		input("XError: ???")
+
+	print(defcode, file=sys.stderr)
+	sleep(0.1)
 	return 0
 
 def show_logo() -> None:
@@ -33,69 +49,95 @@ def show_menu(level: int) -> None:
 	with open(f'./cli/menu/menu-{level}.x') as file:
 		print(file.read())
 
-def help() -> None:
-	pass
-
-def about() -> None:
-	pass
-
 def menu() -> None:
-	level = errorlevel = 0
+	level = 0
+	errorlevel = 0
+	choice = str()
+	filename = str()
+	vault = Vault(filename)
 	while True:
 		show_menu(level)
 
-
 		if level == 0:
-			if errorlevel == 1:
-				errorlevel = show_error(0)
+			try:
+				if errorlevel == 100:
+					errorlevel = show_error(errorlevel)
 
-			choice = input("> ")
+				choice = input("> ")
 
-			if choice in ("1", "2"):
-				level = int(choice)
-			elif choice.lower() in ("0", "exit", "quit"):
-				break
-			else:
-				errorlevel = 1
+				if choice in ("1", "2"):
+					level = int(choice)
+				elif choice.lower() in ("0", "exit", "quit"):
+					break
+				else:
+					errorlevel = 100
+
+			except KeyboardInterrupt:
+				raise
+			except Exception as error:
+				show_error(-99, **globals())
 
 		elif level == 1:
 			try:
+				if errorlevel in (201, 202, 203):
+					errorlevel = show_error(errorlevel, filename=filename)
+
 				filename = input("Enter file in which to save the vault: ")
 
 				if os.path.isfile(filename):
-					show_error(100, filename=filename)
+
+					show_error(200, filename=filename)
 
 					choice = input("Overwrite (y/N)? ")
-					if choice.lower() in ("y", "yes"):
-						errorlevel = 2
-				else:
-					errorlevel = 2
 
-				if errorlevel == 2:
-					vault = Vault(filename); vault.store(data=[])
-					errorlevel = 0
+					if choice.lower() in ("y", "yes"):
+						errorlevel = 10
+
+				else:
+					errorlevel = 10
+
+				if errorlevel == 10:
+					vault = Vault(filename)
+					vault.store(data=[])
 					level = 3
+					errorlevel = 0
+
 			except KeyboardInterrupt:
 				level = 0
-			except:
-				show_error(-1024)
-				pass
+			except FileNotFoundError:
+				errorlevel = 201
+			except PermissionError:
+				errorlevel = 202
+			except IsADirectoryError:
+				errorlevel = 203
+			except Exception as error:
+				show_error(-99, **globals())
+
 		elif level == 2:
 			try:
-				if errorlevel == 1:
-					errorlevel = show_error(200, filename=filename)
+				if errorlevel in (201, 202, 203):
+					errorlevel = show_error(errorlevel, filename=filename)
 
 				filename = input("Enter the file of the vault to import: ")
+
 				if os.path.isfile(filename):
-					vault = Vault(filename); vault.retrieve()
+					vault = Vault(filename)
+					vault.retrieve()
 					level = 3
 				else:
-					errorlevel = 1
+					open(filename)
+
 			except KeyboardInterrupt:
 				level = 0
-			except:
-				show_error(-1024)
-				pass
+			except FileNotFoundError:
+				errorlevel = 201
+			except PermissionError:
+				errorlevel = 202
+			except IsADirectoryError:
+				errorlevel = 203
+			except Exception as error:
+				show_error(-99, **globals())
+
 		elif level == 3:
 			try:
 				input("what's next? ")
