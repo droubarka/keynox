b1c788ba870863033056f0e7fccff4bd03a801aa
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-17 20:48:05 -0400

    Update CLI

--- keynox/cli.py
+++ keynox/cli.py
@@ -5,6 +5,17 @@ from time import sleep
 
 from vault import Vault
 
+CLEAR = "cls" if os.name == "nt" else "clear"
+defcode, redcode = '\033[0m', '\033[91m'
+
+def show_error(errorlevel: int, *args, **kwargs) -> int:
+	if errorlevel == 1:
+		sleep(0.1)
+		print(f"{redcode}[!] Invalid option. Please try again.\n", file=sys.stderr)
+	elif errorlevel == 2:
+		print(f"{redcode}{kwargs['filename']} already exists.", file=sys.stderr)
+	print(defcode, end='')
+	return 0
 
 def show_logo() -> None:
 	# Displays the Keynox logo
@@ -28,11 +39,10 @@ def menu() -> None:
 	while True:
 		show_menu(level)
 
+
 		if level == 0:
 			if errorlevel == 1:
-				errorlevel = 0
-				sleep(0.1)
-				print(f"{redcode}[!] Invalid option. Please try again.\n{defcode}")
+				errorlevel = show_error(errorlevel)
 
 			choice = input("> ")
 
@@ -44,24 +54,29 @@ def menu() -> None:
 				errorlevel = 1
 
 		elif level == 1:
-			print("Enter file in which to save the vault:")
 			try:
-				file_path = input("> ")
-				if os.path.isfile(file_path):
-					print(f"{file_path} already exists.", file=sys.stderr)
+				filename = input("Enter file in which to save the vault: ")
+				if os.path.isfile(filename):
+					show_error(2, filename=filename)
 					choice = input("Overwrite (y/N)? ")
 					if choice.lower() in ("y", "yes"):
-						vault = Vault(file_path)
-						vault.store(data={})
+						errorlevel = 3
 				else:
-					vault = Vault(file_path)
-					vault.store(data={})
+					errorlevel = 3
+
+				if errorlevel == 3:
+					errorlevel = 0
+					level = -1
+					pass
 			except KeyboardInterrupt:
 				level = 0
+		elif level == 2:
+			level = -1
+			pass
 
-# reboot section
-CLEAR = "cls" if os.name == "nt" else "clear"
-defcode, redcode = '\033[0m', '\033[91m'
+		elif level == -1:
+			input("[!] not finished yet.")
+			pass
 
 def main():
 	try:
