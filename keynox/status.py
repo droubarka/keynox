a933ef545e0182851176aed1d9e369a9da631e4d
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-21 06:59:54 -0400

    Update status.py

--- keynox/status.py
+++ keynox/status.py
@@ -3,26 +3,27 @@ import sys
 
 from time import sleep
 
+
 def show_error(errorlevel: int, **args) -> int:
 	defcode, redcode = '\033[0m', '\033[91m' #?
 
-	print("{}[!] ".format(redcode), end='', file=sys.stderr)
+	sys.stderr.write(f"{redcode}")
 
 	if errorlevel == -99:
-		print("#? XError: {}".format(args["error"]), file=sys.stderr)
+		sys.stderr.write("[#?] XError: {}\n".format(args["error"]))
 		sys.exit(1)
 
 	elif errorlevel == 400:
-		print("Invalid choice.", file=sys.stderr)
+		sys.stderr.write("[!] Invalid choice.\n")
 	elif errorlevel == 401:
-		print("PermissionError: {}".format(args["filename"]), file=sys.stderr)
+		sys.stderr.write("[!] PermissionError: {}\n".format(args["filename"]))
 	elif errorlevel == 402:
-		print("IsADirectoryError: {}".format(args["filename"]), file=sys.stderr)
+		sys.stderr.write("[!] IsADirectoryError: {}\n".format(args["filename"]))
 	elif errorlevel == 403:
-		print("Already exists: {}".format(args["filename"]), file=sys.stderr)
+		sys.stderr.write("\n[!] Already exists: {}\n".format(args["filename"]))
 	elif errorlevel == 404:
-		print("FileNotFoundError: {}".format(args["filename"]), file=sys.stderr)
+		sys.stderr.write("[!] FileNotFoundError: {}\n".format(args["filename"]))
 
 	sleep(0.1)
-	print(defcode, file=sys.stderr)
+	sys.stderr.write(f"{defcode}\n")
 	return 200
