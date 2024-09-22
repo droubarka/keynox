8628c71b3a6684e7f29c656b76a1dd147be19b4a
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-21 22:31:57 -0400

    Upgrade ALL

--- keynox/cli.py
+++ keynox/cli.py
@@ -1,226 +1,148 @@
 
-import os, sys
+import os
+import sys
+import time
 
-from getpass import getpass
-from time import sleep, strftime
-from vault import Vault
-from status import show_error
-from password_manager import PasswordManager
-from utils import generate_password as genpass
+from io import open
 
 
-def add_entry(pm: PasswordManager) -> None:
-	entry = {"data": {}, "meta": {}}
+KEYNOX_LOGO_FILEPATH = './cli/keynox-logo.dat'
+MENU_FILEPATH_FORMAT = './cli/menu/lvl-{}.dat'
 
-	choice = input("Generate password randomly (Y/n)? ")
 
-	if choice.lower() in ("n", "no"):
-		password = getpass("password: ") #?
+def render_logo_and_menu() -> None:
+	"""
+	Renders the logo and menu files.
+	"""
 
-	else:
-		policy = {"digit": 1, "lowercase": 1, "uppercase": 1, "special": 1}
-		password = genpass(32, policy)
+	try:
+		with open(KEYNOX_LOGO_FILEPATH) as file:
+			print(file.read())
 
-	entry["data"] = {
-		"url"      : input("url: "),
-		"username" : input("username: "),
-		"password" : password,
-		"name"     : input("name: "),
-		"category" : input("category: "),
-		"notes"    : input("notes: ")
+	except FileNotFoundError: Ellipsis
+
+	try:
+		with open(MENU_FILEPATH_FORMAT.format(level)) as file:
+			print(file.read())
+
+	except FileNotFoundError: Ellipsis
+
+
+def display_error_message(*args) -> None:
+	"""
+	Displays an error message based on the given error level.
+	"""
+
+	error_lookup = {
+		-1 : 'XError',
+		400: 'InvalidOption',
+		401: 'PermissionError',
+		402: 'IsADirectoryError',
+		403: 'FileAlreadyExists',
+		404: 'FileNotFoundError'
 	}
 
-	# meta
-	entry["meta"]["last-update"] = strftime("%a %b %d %H:%M:%S %Z %Y")
+	sys.stderr.write("[!] {}".format(error_lookup[errorlevel]))
 
-	pm.add_entry(entry)
+	if args:
+		sys.stderr.write(": {}".format(*args)) #?
 
-def level_3(vault: Vault) -> tuple:
-	#? Next step (not implemented)
-	errorlevel = 200
-	level = 3
-	pm = PasswordManager(vault)
-	try:
-		choice = input("> ")
-
-		if choice == "1":
-			add_entry(pm)
-			pm.vault.store(pm.entries) #?
-
-		elif choice in ("2", "3", "4"):
-			level = 0 #?
-		else:
-			errorlevel = 400
-
-	except KeyboardInterrupt:
-		level = 0 #?
-	except Exception as error:
-		show_error(-99, error=error) #? Handle unexpected errors
+	sys.stderr.write("\n")
 
 
-	return errorlevel, level
+def handle_error() -> None:
+	"""
+	Handles error levels and displays appropriate error messages.
+	"""
+
+	global error, errorlevel
+
+	redcode = '\033[91m'
+	defcode = '\033[0m'
+
+	if errorlevel == 403:
+		print()
+
+	sys.stderr.write(f"{redcode}")
+	display_error_message(error)
+	sys.stderr.write(f"{defcode}")
+	print()
+	time.sleep(0.1)
+
+	error, errorlevel = None, 200
 
 
-def open_vault(filename: str) -> Vault:
-	vault = Vault(filename)
-	vault.retrieve()
+def main_menu() -> None:
+	"""
+	Displays the main menu and handles user input.
+	"""
 
-	return vault
+	global error, level, errorlevel
 
-def import_vault() -> tuple:
-	#! Import a vault
-	errorlevel = 200
-	level = 2
-	filename = None
-	vault = None
-	try:
-		filename = input("Enter the file of the vault to import: ")
+	choice = input("> ")
 
-		if os.path.isfile(filename):
-			vault = open_vault(filename)
-			level = 3
+	if choice in ("1", "2"):
+		level = int(choice)
 
-		else:
-			# Try to open the file (this will raise an exception)
-			open(filename).close()
+	elif choice.lower() in ("0", "exit"):
+		sys.exit(0)
 
-	except KeyboardInterrupt:
-		level = 0
-	except PermissionError:
-		errorlevel = 401
-	except IsADirectoryError:
-		errorlevel = 402
-	except FileNotFoundError:
-		errorlevel = 404
-	except Exception as error:
-		show_error(-99, error=error) #? Handle unexpected errors
-
-	return errorlevel, level, filename, vault
+	else:
+		error, errorlevel = choice, 400 #: InvalidOption
 
 
-def new_vault(filename: str) -> Vault:
-	vault = Vault(filename)
-	vault.store(data=[])
-
-	return vault
-
-def create_vault() -> tuple:
-	errorlevel = 200
-	level = 1
-	filename = None
-	vault = None
-	try:
-		filename = input("Enter file in which to save the vault: ")
-
-		if os.path.isfile(filename):
-			# Show error message if file already exists
-			show_error(403, filename=filename)
-
-			choice = input("Overwrite (y/N)? ")
-
-			if choice.lower() in ("y", "yes"):
-				vault = new_vault(filename)
-				level = 3
-
-		else:
-			vault = new_vault(filename)
-			level = 3
-
-	except KeyboardInterrupt:
-		level = 0
-	except PermissionError:
-		errorlevel = 401
-	except IsADirectoryError:
-		errorlevel = 402
-	except FileNotFoundError:
-		errorlevel = 404
-	except Exception as error:
-		show_error(-99, error=error) #? Handle unexpected errors
-
-	return errorlevel, level, filename, vault
+#! Un/Stable code
 
 
-def main_menu() -> tuple:
-	errorlevel = 200
-	level = 0
-	try:
-		choice = input("> ")
+def display_menu() -> None: #?
 
-		if choice in ("1", "2"):
-			level = int(choice)
-		elif choice.lower() in ("0", "exit", "quit"):
-			raise KeyboardInterrupt
-		else:
-			# Invalid choice
-			errorlevel = 400
-
-	except KeyboardInterrupt:
-		raise
-	except Exception as error:
-		show_error(-99, error=error) #? Handle unexpected errors
-
-	return errorlevel, level
-
-
-def menu() -> None:
-	errorlevel = 200
-	level = 0
-	filename = None
-	vault = None
 	while True:
-		# Clear the terminal screen
-		os.system("cls" if os.name == "nt" else "clear")
+
+		os.system('cls' if os.name == 'nt' else 'clear')
+
+		render_logo_and_menu()
+
+		if errorlevel != 200:
+			handle_error() #?
 
 		try:
-			# Displays the Keynox logo
-			with open('./cli/keynox-logo.dat') as file:
-				print(file.read())
-
-		except FileNotFoundError:
-			Ellipsis
-
-		try:
-			# Displays the menu at the specified level
-			with open(f'./cli/menu/menu-lvl-{level}.dat') as file:
-				print(file.read())
-
-		except FileNotFoundError:
-			Ellipsis
-
-		if errorlevel in (400,):
-			errorlevel = show_error(errorlevel)
-
-		if errorlevel in (401, 402, 403, 404):
-			errorlevel = show_error(errorlevel, filename=filename)
-
-		try:
-			# Handle different levels of the menu
 			if level == 0:
-				errorlevel, level = main_menu()
+				main_menu()
 			elif level == 1:
-				errorlevel, level, filename, vault = create_vault()
-			elif level == 2:
-				errorlevel, level, filename, vault = import_vault()
-			elif level == 3:
-				errorlevel, level = level_3(vault)
+				pass
 			else:
-				raise KeyboardInterrupt #?
+				pass
 
 		except KeyboardInterrupt:
-			raise
+			pass
 		except Exception as error:
-			show_error(-99, error=error) #? Handle unexpected errors
+			pass
 
 
-def main():
+#! Un/Stable code
+
+error = None
+
+level = 0
+errorlevel = 200
+
+def main() -> int:
+	"""
+	Main entry point for the program.
+	"""
+
+	exit_status = 0
+
 	try:
-		menu()
+		display_menu()
+		pass
 
-	except KeyboardInterrupt:
-		print("\n#? Exiting ...")
-		sys.exit(0)
-	except Exception as error:
-		show_error(-99, error=error) #? Handle unexpected errors
+	except SystemExit as exc:
+		exit_status = exc.code
+
+	return exit_status
 
 
-if __name__ == "__main__": main()
+if __name__ == '__main__':
+	exit_status = main()
+	sys.exit(exit_status)
