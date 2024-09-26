de8e527086ba455b0024e55d7533a2cbce0f0748
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-26 19:40:27 -0400

    Change algorithm from PBKDF2HMAC to Scrypt

--- keynox/vault.py
+++ keynox/vault.py
@@ -4,8 +4,8 @@ import json
 import secrets
 
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
+
 
 class Vault:
 	"""
@@ -24,12 +24,8 @@ class Vault:
 		"""
 		Derives an encryption key from the master password and salt.
 		"""
-		kdf = PBKDF2HMAC(
-			algorithm=hashes.SHA256(),
-			length=32,
-			salt=salt,
-			iterations=341291,
-		)
+		#! memory required: 128 * N * r * p == 64MB (67108864 Bytes)
+		kdf = Scrypt(salt=salt, length=32, n=2**16, r=8, p=1)
 		key = base64.urlsafe_b64encode(
 			kdf.derive(self.master_password.encode())
 		)
