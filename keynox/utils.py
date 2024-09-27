7aa4f9a775d0fc77b74645dd76d549120e66b89f
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-26 22:21:53 -0400

    Update utils/genpass

--- keynox/utils.py
+++ keynox/utils.py
@@ -1,32 +1,67 @@
 
 import string
 
-from secrets import choice, SystemRandom
+from secrets import SystemRandom
+
+
+def choices(population: list, *args, **kwargs) -> list:
+	"""
+	Return a k sized list of population elements chosen with replacement.
+
+	If the relative weights or cumulative weights are not specified,
+	the selections are made with equal probability.
+	"""
+	return SystemRandom().choices(population, *args, **kwargs)
+
+
+def shuffle(x: list) -> None:
+	"""
+	Shuffle list x in place, and return None.
+	"""
+	return SystemRandom().shuffle(x)
 
 
 def genpass(length: int, policy: dict) -> str:
+	"""
+	Generates a password based on the given policy.
+	"""
 
 	if length < sum(policy.values()):
-		raise ValueError("Length must be greater than or equal to the sum of policy values.")
-
-	chars = string.digits + string.ascii_letters + string.punctuation
+		raise ValueError(
+			"Length must be greater than or equal to the sum of policy values."
+		)
 
 	password = []
 
+	chars = string.digits + string.ascii_letters + string.punctuation
+
+	password.extend(
+		choices(chars, k=(length - sum(policy.values())))
+	)
+
 	for char_type, count in policy.items():
 		if char_type == "digit":
-			password.extend([choice(string.digits) for _ in range(count)])
+			password.extend(
+				choices(string.digits, k=count)
+			)
 		elif char_type == "lowercase":
-			password.extend([choice(string.ascii_lowercase) for _ in range(count)])
+			password.extend(
+				choices(string.ascii_lowercase, k=count)
+			)
 		elif char_type == "uppercase":
-			password.extend([choice(string.ascii_uppercase) for _ in range(count)])
+			password.extend(
+				choices(string.ascii_uppercase, k=count)
+			)
 		elif char_type == "special":
-			password.extend([choice(string.punctuation) for _ in range(count)])
+			password.extend(
+				choices(string.punctuation, k=count)
+			)
 		else:
-			raise ValueError(f"Invalid policy type '{char_type}' - use digit, lowercase, uppercase, special.")
+			raise ValueError(
+					f"Invalid policy type '{char_type}'"
+					" - use digit, lowercase, uppercase or special."
+			)
 
-	password.extend([choice(chars) for _ in range(length - sum(policy.values()))])
-
-	SystemRandom().shuffle(password)
+	shuffle(password)
 
 	return "".join(password)
