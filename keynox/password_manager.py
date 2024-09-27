13d1bf549b4c8b9728fb5fde516500cb432cdeb9
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-26 20:50:28 -0400

    Update PasswordManager

--- keynox/password_manager.py
+++ keynox/password_manager.py
@@ -3,23 +3,54 @@ from vault import Vault
 
 
 class PasswordManager:
+	"""
+	A class to manage password entries stored in a vault.
+	"""
+
 	def __init__(self, vault: Vault) -> None:
-		self.vault, self.entries = vault, vault.retrieve()
+		"""
+		Initializes the PasswordManager object with a vault.
+		"""
+		self.vault = vault
+		self.entries = vault.retrieve()
+
 
 	def add_entry(self, entry: dict) -> None:
+		"""
+		Adds a new entry to the list os password entries.
+		"""
 		self.entries.append(entry)
 
+
 	def remove_entry(self, entry: dict) -> None:
+		"""
+		Removing an existing entry from the list of password entries.
+		"""
 		self.entries.remove(entry)
 
+
+	def update_vault(self) -> None:
+		"""
+		Saves the current entries to the vault.
+		"""
+		self.vault.store(data=self.entries)
+
+
 	def get_entry(self, key: str, value: str) -> dict:
+		"""
+		Retrieves a single entry that matches the given key-value pair.
+		"""
 		for entry in self.entries:
 			if key in entry["data"].keys():
 				if entry["data"][key] == value:
 					return entry
 		return None
 
+
 	def get_entry_list(self, key: str, value: str) -> list:
+		"""
+		Retrieves a list of entries that match the given key-value pair.
+		"""
 		list = []
 		for entry in self.entries:
 			if key in entry["data"].keys():
