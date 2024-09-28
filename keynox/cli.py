e2b9a95b0820acd4abb07b84c04671382abd1a3a
Mohamed A. Oubarka <droubarka@gmail.com>
2024-09-28 17:12:59 -0400

    Add CLI/MAIN-MENU

--- keynox/cli.py
+++ keynox/cli.py
@@ -1,44 +1,122 @@
 
+"""
+Keynox CLI Program
+
+This program provides a command-line interface for managing passwords.
+"""
+
+import os
+import sys
+
+from colorama import Fore
 from io import open
 
 
 KEYNOX_LOGO_FILEPATH = './cli/keynox-logo.dat'
 MENU_FILEPATH_FORMAT = './cli/menu/lvl-{}.dat'
 
+password_manager = None
 
-def render_logo_and_menu() -> None:
+
+def render_logo_and_menu(level: int=None) -> None:
 	"""
 	Renders the logo and menu files.
 	"""
+
+	os.system('cls' if os.name == 'nt' else 'clear')
 
 	try:
 		with open(KEYNOX_LOGO_FILEPATH, 'rt') as file:
 			print(file.read())
 
-	except FileNotFoundError: Ellipsis
+	except Exception: Ellipsis
 
 	if password_manager != None:
-		print(f"\tVault: {password_manager.vault.filename}")
-		is_synchronized = password_manager.sync_fingerprint == password_manager.calculate_sync_fingerprint()
-		print(f"\tSynchronized: {is_synchronized}", end=' | ')
-		print(f"Entries: {len(password_manager.entries)}", end='')
-		print("\n")
+		pass
 
 	try:
-		with open(MENU_FILEPATH_FORMAT.format(level), 'rt') as file:
-			print(file.read())
+		if level != None:
+			with open(MENU_FILEPATH_FORMAT.format(level), 'rt') as file:
+				print(file.read())
 
-	except FileNotFoundError: Ellipsis
+	except Exception: Ellipsis
 
 
-def display_error_message() -> None:
+def display_error_message(error: Exception) -> None:
 	"""
-	Displays an error message based on the given error level.
+	Displays an error message.
 	"""
 
-	pass
+	sys.stderr.write(Fore.RED)
+	sys.stderr.write(f"[!] {type(error).__name__}: {error}\n")
+	sys.stderr.write(Fore.RESET)
+	print()
+
+	return None
 
 
-level = 0
+def main_menu() -> int:
+	"""
+	Displays the main menu.
+	"""
 
-password_manager = None
+	choice = input("> ")
+
+	if choice.lower() in ("0", "exit"):
+		sys.exit(0)
+
+	elif choice in ("1", "2"):
+		return int(choice)
+
+	else:
+		raise ValueError(f"Invalid choice: '{choice}'")
+
+
+def display_menu() -> None:
+	"""
+	Displays the menu and manages user interactions.
+	"""
+
+	level = 0
+	error = None
+
+	while True:
+		render_logo_and_menu(level)
+
+		if error != None:
+			error = display_error_message(error)
+
+		if level == 0:
+			try:
+				level = main_menu()
+
+			except ValueError as xerr:
+				error = xerr
+
+			except KeyboardInterrupt:
+				sys.exit(0)
+
+		else:
+			sys.exit(0)
+
+
+def main() -> int:
+	"""
+	Main entry point for the program.
+	"""
+
+	exit_status = 0
+
+	try:
+		display_menu()
+		pass
+
+	except SystemExit as exc:
+		exit_status = exc.code
+
+	return exit_status
+
+
+if __name__ == '__main__':
+	exit_status = main()
+	sys.exit(exit_status)
